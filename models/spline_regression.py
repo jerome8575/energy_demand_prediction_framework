383c7c8aece3ba2f4f2c768dbacf0cbfc1e42b80
Jerome Emery <58118521+jerome8575@users.noreply.github.com>
2023-06-12 13:06:07 -0400

    Regression splines improvement

--- models/spline_regression.py
+++ models/spline_regression.py
@@ -5,16 +5,19 @@ import datetime
 import numpy as np
 import pandas as pd
 from patsy import dmatrix
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, MinMaxScaler
 import statsmodels.api as sm
 from data.quebec_energy_demand import HQ_data
+from statsmodels.tsa.arima.model import ARIMA
+import matplotlib.pyplot as plt
 
 
 class SplineRegression:
 
     def get_predictions(self, data, train_start, train_end, test_start, test_end):
 
-        pf = PolynomialFeatures(degree=2)
+        pf = PolynomialFeatures(degree=1)
+        scaler = MinMaxScaler()
 
         # get lagged demand
         lagged_demand = list(data.loc[train_start:test_end - datetime.timedelta(days=1), "demand"])
@@ -24,31 +27,38 @@ class SplineRegression:
         data["lagged_demand"] = np.log(lagged_demand)
 
         forecasts = []
+
         for h in range(24):
             hourly_data = data[h::24]
 
-            temp = np.array(hourly_data.loc[:, "temp"])
-            basis_x = dmatrix("bs(temp, knots=(15, 22), degree=3, include_intercept=False)", {"temp": temp}, return_type='dataframe')
-            basis_x["lags"] = list(hourly_data.loc[:, "lagged_demand"])
-            fourier = pd.DataFrame(np.array(self.get_fourier_features(6, 7, hourly_data.loc[:, "day"])))
-    
-            # train test split
-            features = pd.concat([basis_x, fourier], ignore_index=True, axis=1)
+            temp = scaler.fit_transform(np.array(hourly_data.loc[:, "temp"]).reshape(-1, 1)).flatten()
+            basis_x = dmatrix("bs(temp, knots=(0.65, 0.8), degree=1, include_intercept=False)", {"temp": temp}, return_type='dataframe')
+
             target = np.log(np.array(hourly_data.loc[:, "demand"]))
 
-            train_features = pf.fit_transform(features[:-1])
+            train_features = pf.fit_transform(basis_x[:-1])
             train_target = target[:-1]
 
             endog = train_target
 
-            model = sm.GLM(endog, train_features).fit()
+            model = sm.OLS(endog, train_features).fit()
 
             # get predictions
+            training_forecasts = model.predict(train_features)
+            residuals = training_forecasts - train_target
 
-            test_features = pf.fit_transform(np.array(features.loc[len(features) -1, :]).reshape(1, -1))
 
-            forecast = model.predict(test_features).tolist()
+
+            res_model = ARIMA(residuals[-50:], order=(1, 0, 1), exog=temp[-51:-1]).fit()
+            training_res_hat = res_model.predict(0, len(residuals[-50:]) - 1, exog=temp[-51:-1])
+ 
+            res_hat = res_model.forecast(1, exog=temp[-1])
+            print(res_hat)
+            test_features = pf.fit_transform(np.array(basis_x.loc[len(basis_x) -1, :]).reshape(1, -1))
+
+            forecast = model.predict(test_features).tolist() #- res_hat
             forecasts.append(forecast)
+
 
         return np.exp(np.array(forecasts).flatten())
 
@@ -67,14 +77,4 @@ class SplineRegression:
         return fourier_features
     
 
-train_start = datetime.datetime(2021, 1, 1, 0, 0, 0)
-train_end = datetime.datetime(2021, 12, 31, 23, 0, 0)
-test_start = datetime.datetime(2022, 1, 1, 0, 0,0)
-test_end = datetime.datetime(2022, 1, 1, 23, 0, 0)
-
-data = HQ_data().get_history()
-
-sr = SplineRegression()
-predictions = sr.get_predictions(data, train_start, train_end, test_start, test_end)
-print(predictions)
 
--- simulation/run_simulation.py
+++ simulation/run_simulation.py
@@ -2,12 +2,12 @@
 from simulation import Simulation
 import datetime
 
-train_start = datetime.datetime(2021, 1, 1, 0, 0, 0)
+train_start = datetime.datetime(2021, 1, 1, 1, 0, 0)
 train_end = datetime.datetime(2021, 12, 31, 23, 0, 0)
 test_start = datetime.datetime(2022, 1, 1, 0, 0,0)
 test_end = datetime.datetime(2022, 1, 1, 23, 0, 0)
 
-sim = Simulation(3, train_start, train_end, test_start, test_end)
+sim = Simulation(30, train_start, train_end, test_start, test_end)
 forecasts = sim.run_simulation()
 sim.plot_sim_results(forecasts)
 
